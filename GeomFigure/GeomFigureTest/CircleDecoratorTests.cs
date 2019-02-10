352654ac24317d9ee316d4db27d1353d66d67c04
yahya <yahyaislamov15@mail.ru>
2019-02-11 00:31:53 +0300

    завершил геометрические фигуры

---
+++ GeomFigure/GeomFigureTest/CircleDecoratorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeomFigure.Decorator.WorkWithFigures;
+
+namespace GeomFigureTest
+{
+    [TestClass]
+    public class CircleDecoratorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
---
+++ GeomFigure/GeomFigureTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GeomFigureTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeomFigureTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e9e2e0a-d3ae-459a-9897-d2ab5cb97437")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
